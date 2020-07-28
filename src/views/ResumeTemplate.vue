863ec9fc8b383f4d6e3a509f37a726301d3c3621
Liva Raita <liva.raita@gmail.com>
2020-07-28 18:02:23 +0300

    add html from previous cv project

---
+++ src/views/ResumeTemplate.vue
@@ -0,0 +1,202 @@
+<template>
+  <div class="container">
+    <section class="header">
+      <h1 contenteditable="true" placeholder="Full Name">
+        {{ fullName }}
+      </h1>
+      <h2 contenteditable="true" placeholder="Title"></h2>
+      <div class="contacts">
+        <a href="" contenteditable="true" placeholder="website"></a>
+        <a href="" contenteditable="true" placeholder="email"></a>
+        <a href="" contenteditable="true" placeholder="phone"></a>
+      </div>
+    </section>
+    <hr />
+    <div class="content">
+      <div class="affiliations">
+        <p
+          class="short-description"
+          contenteditable="true"
+          placeholder="Add a short description (5-10 sentences) focusing on your experience and the
+        future carrier direction you're aiming for."
+        ></p>
+        <div class="experience">
+          <h2>Experience</h2>
+          <div class="position-wrapper">
+            <div class="position-header">
+              <h3>
+                <span contenteditable="true" placeholder="Position"></span> at
+                <span contenteditable="true" placeholder="Company"></span>
+              </h3>
+              <p class="date-location">
+                <span contenteditable="true" placeholder="Start Date"></span>
+                - <span contenteditable="true" placeholder="End Date"></span>,
+                <span contenteditable="true" placeholder="Location"></span>
+              </p>
+            </div>
+            <p
+              class="position-description"
+              contenteditable="true"
+              placeholder="Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni"
+            ></p>
+          </div>
+          <div class="position-wrapper">
+            <div class="position-header">
+              <h3>
+                <span contenteditable="true" placeholder="Position"></span> at
+                <span contenteditable="true" placeholder="Company"></span>
+              </h3>
+              <p class="date-location">
+                <span contenteditable="true" placeholder="Start Date"></span>
+                - <span contenteditable="true" placeholder="End Date"></span>,
+                <span contenteditable="true" placeholder="Location"></span>
+              </p>
+            </div>
+            <p
+              class="position-description"
+              contenteditable="true"
+              placeholder="Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni"
+            ></p>
+          </div>
+          <div class="position-wrapper">
+            <div class="position-header">
+              <h3>
+                <span contenteditable="true" placeholder="Position"></span> at
+                <span contenteditable="true" placeholder="Company"></span>
+              </h3>
+              <p class="date-location">
+                <span contenteditable="true" placeholder="Start Date"></span>
+                - <span contenteditable="true" placeholder="End Date"></span>,
+                <span contenteditable="true" placeholder="Location"></span>
+              </p>
+            </div>
+            <p
+              class="position-description"
+              contenteditable="true"
+              placeholder="Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni"
+            ></p>
+          </div>
+        </div>
+        <div class="education">
+          <h2>Education</h2>
+          <div class="position-wrapper">
+            <div class="position-header">
+              <h3>
+                <span
+                  contenteditable="true"
+                  placeholder="University, Courses, Certifications etc."
+                ></span>
+                at
+              </h3>
+              <p class="date-location">
+                <span
+                  contenteditable="true"
+                  placeholder="Graduation Date"
+                ></span
+                >,
+                <span contenteditable="true" placeholder="Location"></span>
+              </p>
+            </div>
+            <p
+              class="position-description"
+              contenteditable="true"
+              placeholder="Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni"
+            ></p>
+          </div>
+        </div>
+      </div>
+      <div class="skills">
+        <h2>Skills</h2>
+        <div class="category">
+          <h3 contenteditable="true" placeholder="Category One"></h3>
+          <div class="skill-groups">
+            <span
+              class="skill-group"
+              contenteditable="true"
+              placeholder="First skill"
+            ></span>
+            <span
+              class="skill-group"
+              contenteditable="true"
+              placeholder="Second skill"
+            ></span>
+            <span
+              class="skill-group"
+              contenteditable="true"
+              placeholder="Third skill"
+            ></span>
+            <span
+              class="skill-group"
+              contenteditable="true"
+              placeholder="Fourth skill"
+            ></span>
+          </div>
+        </div>
+        <div class="category">
+          <h3 contenteditable="true" placeholder="Category Two"></h3>
+          <div class="skill-groups">
+            <span
+              class="skill-group"
+              contenteditable="true"
+              placeholder="First skill"
+            ></span>
+            <span
+              class="skill-group"
+              contenteditable="true"
+              placeholder="Second skill"
+            ></span>
+            <span
+              class="skill-group"
+              contenteditable="true"
+              placeholder="Third skill"
+            ></span>
+            <span
+              class="skill-group"
+              contenteditable="true"
+              placeholder="Fourth skill"
+            ></span>
+          </div>
+        </div>
+        <div class="category">
+          <h3 contenteditable="true" placeholder="Category Three"></h3>
+          <div class="skill-groups">
+            <span
+              class="skill-group"
+              contenteditable="true"
+              placeholder="First skill"
+            ></span>
+            <span
+              class="skill-group"
+              contenteditable="true"
+              placeholder="Second skill"
+            ></span>
+            <span
+              class="skill-group"
+              contenteditable="true"
+              placeholder="Third skill"
+            ></span>
+            <span
+              class="skill-group"
+              contenteditable="true"
+              placeholder="Fourth skill"
+            ></span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  data() {
+    return {
+      fullName: ""
+    };
+  },
+  name: "ResumeTemplate",
+  components: {}
+};
+</script>
