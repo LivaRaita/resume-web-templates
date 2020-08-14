aeef38339a4ca14a0eb76cb790a06a597c1e8a62
Liva Raita <liva.raita@gmail.com>
2020-08-14 21:02:46 +0300

    add experiments with new layout

--- src/store/index.js
+++ src/store/index.js
@@ -4,7 +4,11 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    cvTemplate: {
+      fullName: ""
+    }
+  },
   mutations: {},
   actions: {},
   modules: {}
--- src/views/ResumeEdit.vue
+++ src/views/ResumeEdit.vue
@@ -1,46 +1,57 @@
 <template>
-  <div class="container">
-    <div class="resume-section">
-      <h3 class="group-heading">Personal Details</h3>
-      <div class="row">
-        <div class="form-group">
-          <label for="">Full Name</label>
-          <input type="text" />
+  <div class="wrapper">
+    <div class="form-container">
+      <div class="resume-section">
+        <h3 class="group-heading">Personal Details</h3>
+        <div class="row">
+          <div class="form-group">
+            <label for="">Full Name</label>
+            <input type="text" v-model="fullName" />
+          </div>
+          <div class="form-group">
+            <label for="">Job Title</label>
+            <input type="text" />
+          </div>
         </div>
-        <div class="form-group">
-          <label for="">Job Title</label>
-          <input type="text" />
+      </div>
+
+      <div class="resume-section">
+        <h3 class="group-heading">Websites & Social Links</h3>
+        <div class="section-outline">
+          <div class="section-top">
+            <h2>(Not specified)</h2>
+            <svg
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M16.59 8.58997L12 13.17L7.41 8.58997L6 9.99997L12 16L18 9.99997L16.59 8.58997Z"
+                fill="black"
+                fill-opacity="0.54"
+              />
+            </svg>
+          </div>
+          <div class="row">
+            <div class="form-group">
+              <label for="">Label</label>
+              <input type="text" />
+            </div>
+            <div class="form-group">
+              <label for="">Link</label>
+              <input type="text" />
+            </div>
+          </div>
         </div>
       </div>
     </div>
-
-    <div class="resume-section">
-      <h3 class="group-heading">Websites & Social Links</h3>
-      <div class="section-outline">
-        <div class="section-top">
-          <h2>(Not specified)</h2>
-          <svg
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M16.59 8.58997L12 13.17L7.41 8.58997L6 9.99997L12 16L18 9.99997L16.59 8.58997Z"
-              fill="black"
-              fill-opacity="0.54"
-            />
-          </svg>
-        </div>
-        <div class="row">
-          <div class="form-group">
-            <label for="">Label</label>
-            <input type="text" />
-          </div>
-          <div class="form-group">
-            <label for="">Link</label>
-            <input type="text" />
+    <div class="preview">
+      <div class="resume-peview-wrapper">
+        <div class="resume-preview">
+          <div class="resume-content">
+            <h1>{{ fullName }}</h1>
           </div>
         </div>
       </div>
@@ -49,10 +60,26 @@
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      fullName: ""
+    };
+  }
+};
 </script>
 
 <style>
+.wrapper {
+  display: flex;
+}
+.form-container {
+  display: flex;
+  flex-direction: column;
+  width: 50%;
+  padding: 24px;
+  max-width: 700px;
+}
 .section-top h2 {
   font-size: 14px;
   line-height: 20px;
@@ -100,5 +127,41 @@ export default {};
   border: 1px solid #dddbda;
   border-radius: 4px;
   padding: 16px;
+}
+
+.preview {
+  position: fixed;
+  right: 0;
+  top: 0;
+  width: 50%;
+  height: 100%;
+  background: #dadada;
+  box-shadow: inset 0px 4px 24px rgba(0, 0, 0, 0.2);
+  padding: 22px;
+  overflow-y: scroll;
+}
+/* .resume-peview-wapper {
+ 
+} */
+
+.resume-preview {
+  position: relative;
+  width: 100%;
+  padding-top: 129%;
+  background-color: white;
+  border-radius: 2px;
+  margin-right: 22px;
+  margin-bottom: 24px;
+}
+
+.resume-content {
+  position: absolute;
+  top: 45px;
+  left: 44px;
+}
+
+.resume-content h1 {
+  font-size: calc(14px + (26 - 14) * ((100vw - 300px) / (1600 - 300)));
+  line-height: calc(1.3em + (1.5 - 1.2) * ((100vw - 300px) / (1600 - 300)));
 }
 </style>
